8f360dc05241c2cb484d8c2ce105db8595e73bfc
jianpingbadao <weida.ustc@gmail.com>
2018-02-17 14:56:45 -0500

    create homepage

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VehSense</title>
+</head>
+
+<body>
+    <h1 align="center">Welcome to VehSense!</h1>
+
+    <?php
+    include "utility.php";
+
+    if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+        $uri = 'https://';
+    } else {
+        $uri = 'http://';
+    }
+    // $uri .= $_SERVER['HTTP_HOST'];
+
+    date_default_timezone_set("America/New_York");
+    echo "Current time (ET) is: " . date("h:i:sa"), "<br>";
+
+    // echo exec('whoami');
+
+    exit;
+    ?>
+
+</body>
+</html>
